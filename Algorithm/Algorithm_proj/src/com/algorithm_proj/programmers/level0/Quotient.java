cf2a7e813e513551af10be29905ca907d4e95b3e
moveuk <v_donguk@naver.com>
2022-10-22 23:37:03 +0900

    프로그래머스 - level 0 : 몫 구하기

---
+++ Algorithm/Algorithm_proj/src/com/algorithm_proj/programmers/level0/Quotient.java
@@ -0,0 +1,12 @@
+package com.algorithm_proj.programmers.level0;
+
+public class Quotient {
+    // 몫 구하기
+    // https://school.programmers.co.kr/learn/courses/30/lessons/120805
+
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 / num2;
+        return answer;
+    }
+}
