6c25df5d00da2f5fc45cbb11661319925d1df92e
moveuk <v_donguk@naver.com>
2023-12-04 15:19:02 +0900

    Kotlin 계산기 - LV1. Calculator 클래스와 덧셈, 뺄셈, 곱셈, 나눗셈 기능 추가

--- Language/Kotlin/calculator/src/main/kotlin/main.kt
+++ Language/Kotlin/calculator/src/main/kotlin/main.kt
@@ -1,3 +1,27 @@
 fun main() {
-    println("hello")
+    // Lv1 : 계산기 클래스를 만들어 더하기, 빼기, 나누기, 곱하기 연산하기
+    var int1 = 4
+    var int2 = 2
+    var calculator = Calculator()
+
+    println("int1과 int2의 덧셈은 ${calculator.add(int1, int2)} 입니다.")   // 6
+    println("int1과 int2의 뺄셈은 ${calculator.subtract(int1, int2)} 입니다.")  // 2
+    println("int1과 int2의 곱셈은 ${calculator.multiply(int1, int2)} 입니다.")  // 8
+    println("int1과 int2의 나눗셈은 ${calculator.divide(int1, int2)} 입니다.")   // 2
+}
+
+// Lv1 : 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들고, 클래스를 이용하여 연산을 진행하고 출력하기
+class Calculator {
+    fun add(int1: Int, int2: Int): Int {
+        return int1 + int2;
+    }
+    fun subtract(int1: Int, int2: Int): Int {
+        return int1 - int2;
+    }
+    fun multiply(int1: Int, int2: Int): Int {
+        return int1 * int2;
+    }
+    fun divide(int1: Int, int2: Int): Int {
+        return int1 / int2;
+    }
 }
