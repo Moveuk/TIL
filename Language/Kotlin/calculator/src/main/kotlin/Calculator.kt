32c195cd7e3211533e7f8edafd5328a01bdd2a11
moveuk <v_donguk@naver.com>
2023-12-05 17:01:19 +0900

    Kotlin 계산기 LV 4. AbstractOperation, Calculator 수정 AbstractOperation을 operate의 인자로 넣어 사용 가능하도록 수정함.

--- Language/Kotlin/calculator/src/main/kotlin/Calculator.kt
+++ Language/Kotlin/calculator/src/main/kotlin/Calculator.kt
@@ -1,8 +1,9 @@
-class Calculator(var operation: AbstractOperation) {
-    fun operate(int1: Int, int2: Int): Double {
-        return operation.operate(int1, int2);
+class Calculator() {
+    fun operate(operation: AbstractOperation, int1: Int, int2: Int): Double {
+        return operation.operate(int1, int2)
     }
+
     fun remain(int1: Int, int2: Int): Int {
-        return int1 % int2;
+        return int1 % int2
     }
 }
