93c1feaee5ec9f5ae262e6d09c549b006c67cc7d
moveuk <v_donguk@naver.com>
2023-12-12 23:05:03 +0900

    add: webSocket 예제 작성

--- Language/javascript/websocket-zoom/src/public/js/app.js
+++ Language/javascript/websocket-zoom/src/public/js/app.js
@@ -1,1 +1,45 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const socket = new WebSocket(`ws://${window.location.host}`); //클라이언트에서 웹소켓을 서버로 연결하는 코드.
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+function handleOpen() {
+  //클라이언트 쪽에서 열렸을때 작동.
+  console.log("Connected to Server");
+}
+
+socket.addEventListener("open", handleOpen);
+
+//클라이언트 쪽에서 메세지 받았을 때 작동.
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+//클라이언트 쪽에서 연결이 닫혔을 때 작동.
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- Language/javascript/websocket-zoom/src/server.js
+++ Language/javascript/websocket-zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,4 +10,35 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); // 같은 포트(3000)에서 http, ws server 두개 모두 동작하도록 설정.
+
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+//face db 몇 명의 클라이언트가 붙었는지 저장함.
+const sockets = [];
+
+// 클라이언트-서버 연결되면(connection) fn 실행.
+wss.on("connection", (socket) => {
+  sockets.push(socket); // 새로 연결된 소켓 저장.
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
