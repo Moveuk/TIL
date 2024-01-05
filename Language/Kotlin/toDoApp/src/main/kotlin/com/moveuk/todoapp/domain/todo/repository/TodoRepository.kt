7a58bbe9997791011bbb1a4a76bbbf4a23705804
moveuk <v_donguk@naver.com>
2024-01-05 19:48:38 +0900

    ✨Fix: [Kotlin] ToDoApp fix TodoRepository due to change User Entity structure
    1. jpql fetch join 작성
        - t.author.profile.nickname = :author

--- Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/todo/repository/TodoRepository.kt
+++ Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/todo/repository/TodoRepository.kt
@@ -4,17 +4,21 @@ import com.moveuk.todoapp.domain.todo.model.Todo
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface TodoRepository : JpaRepository<Todo, Long> {
 
+    @Query("SELECT t FROM Todo t join fetch t.author where t.author.profile.nickname = :author")
     fun findAllByAuthor(author: String, pageable: Pageable) : Page<Todo>
 
     fun findAllByOrderByCreatedDateDesc(pageable: Pageable) : Page<Todo>
 
     fun findAllByOrderByCreatedDateAsc(pageable: Pageable): Page<Todo>
 
+    @Query("SELECT t FROM Todo t join fetch t.author where t.author.profile.nickname = :author order by t.createdDate desc")
     fun findAllByAuthorOrderByCreatedDateDesc(author: String, pageable: Pageable) : Page<Todo>
 
+    @Query("SELECT t FROM Todo t join fetch t.author where t.author.profile.nickname = :author order by t.createdDate asc")
     fun findAllByAuthorOrderByCreatedDateAsc(author: String, pageable: Pageable) : Page<Todo>
 
 }
