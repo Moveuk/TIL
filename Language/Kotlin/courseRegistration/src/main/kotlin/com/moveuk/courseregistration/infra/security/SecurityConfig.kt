32369f9fa4c4ce02326f99f3a7df1b7f632a17e0
moveuk <v_donguk@naver.com>
2024-01-16 13:50:33 +0900

    add: [Kotlin] Security 기본 세팅

---
+++ Language/Kotlin/courseRegistration/src/main/kotlin/com/moveuk/courseregistration/infra/security/SecurityConfig.kt
@@ -0,0 +1,21 @@
+package com.moveuk.courseregistration.infra.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity //HTTP 통신을 할 때 관련 보안 기능을 켜주는 어노테이션
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .build()
+    }
+}
