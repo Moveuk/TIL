292fe508afe006c9e3bfcd9e0d94790891a329ff
moveuk <v_donguk@naver.com>
2024-01-17 23:49:00 +0900

    ✨Feat: [Kotlin] ToDoApp 기능 별 인가 처리, 예외 처리
    1. SecurityConfig 필터, 예외 처리 추가
    2. todo, reply controller 리팩토링 및 로직 수정
    3. todo, reply Service 리팩토링 및 로직 수정
    4. 인가 처리 예외 커스텀 Handler 작성

--- Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/todo/service/TodoService.kt
+++ Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/todo/service/TodoService.kt
@@ -1,24 +1,103 @@
 package com.moveuk.todoapp.domain.todo.service
 
+import com.moveuk.todoapp.domain.exception.AuthorizationException
+import com.moveuk.todoapp.domain.exception.ModelNotFoundException
 import com.moveuk.todoapp.domain.todo.dto.todo.*
-import com.moveuk.todoapp.domain.user.model.User
+import com.moveuk.todoapp.domain.todo.model.Todo
+import com.moveuk.todoapp.domain.todo.model.toResponse
+import com.moveuk.todoapp.domain.todo.repository.TodoRepository
+import com.moveuk.todoapp.domain.todocard.repository.UserRepository
+import com.moveuk.todoapp.infra.security.UserPrincipal
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
-interface TodoService {
+@Service
+class TodoService(
+    private val todoRepository: TodoRepository,
+    private val userRepository: UserRepository
+) {
     fun getAllTodos(
         sortProperty: SortProperty?,
         sortOrder: SortOrder?,
         author: String?,
         pageNumber: Int?
-    ): Page<TodoResponse>
+    ): Page<TodoResponse> {
+        //Pageable 객체
+        val pageable = PageRequest.of(pageNumber ?: 0, 10)
+        // 할 일 카드 목록 조회
+        val response: Page<TodoResponse> =
+            // sort X, filter X
+            if (sortProperty == null && author == null) todoRepository.findAll(pageable).map { it.toResponse() }
+            // sort X, filter O
+            else if (sortProperty == null && author != null) todoRepository.findAllByAuthor(author, pageable)
+                .map { it.toResponse() }
+            else if (author == null) {
+                // sort O, filter X
+                if (sortOrder == SortOrder.ASC) todoRepository.findAllByOrderByCreatedDateAsc(pageable)
+                    .map { it.toResponse() }
+                else todoRepository.findAllByOrderByCreatedDateDesc(pageable).map { it.toResponse() }
+            } else {
+                // sort O, filter O
+                if (sortOrder == SortOrder.ASC) todoRepository.findAllByAuthorOrderByCreatedDateAsc(author, pageable)
+                    .map { it.toResponse() }
+                else todoRepository.findAllByAuthorOrderByCreatedDateDesc(author, pageable).map { it.toResponse() }
+            }
+        return response
+    }
 
-    fun getTodoByID(todoId: Long): TodoResponse
+    fun getTodoByID(todoId: Long): TodoResponse {
+        return todoRepository.findByIdOrNull(todoId)?.toResponse() ?: throw ModelNotFoundException("Todo", todoId)
+    }
 
-    fun createTodo(request: CreateTodoRequest, authenticatedUser: User): TodoResponse
+    @Transactional
+    fun createTodo(request: CreateTodoRequest, principal: UserPrincipal): TodoResponse {
+        return userRepository.findByIdOrNull(principal.id)!!.let {
+            todoRepository.save(
+                Todo(
+                    title = request.title,
+                    description = request.description,
+                    author = it
+                )
+            ).toResponse()
+        }
+    }
 
-    fun updateTodo(todoId: Long, request: UpdateTodoRequest, authenticatedUser: User): TodoResponse
+    @Transactional
+    fun updateTodo(todoId: Long, request: UpdateTodoRequest, principal: UserPrincipal): TodoResponse {
+        val todo: Todo =
+            todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("Todo", todoId)
 
-    fun deleteTodo(todoId: Long, authenticatedUser: User)
+        if (todo.author.id != principal.id) throw AuthorizationException("수정 권한이 없습니다.")
 
-    fun changeCompletionState(todoId: Long, completionState: Boolean, authenticatedUser: User): TodoResponse
+        val (title, description) = request
+
+        todo.title = title
+        todo.description = description
+
+        return todoRepository.save(todo).toResponse()
+    }
+
+    @Transactional
+    fun deleteTodo(todoId: Long, principal: UserPrincipal) {
+        val todo: Todo =
+            todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("Todo", todoId)
+        if (todo.author.id != principal.id) throw AuthorizationException("삭제 권한이 없습니다.")
+        todoRepository.delete(todo)
+    }
+
+    @Transactional
+    fun changeCompletionState(todoId: Long, completionState: Boolean, principal: UserPrincipal): TodoResponse {
+        val todo: Todo =
+            todoRepository.findByIdOrNull(todoId) ?: throw ModelNotFoundException("Todo", todoId)
+
+        if (todo.author.id != principal.id) throw AuthorizationException("수정 권한이 없습니다.")
+
+        todo.completion = completionState
+
+        return todoRepository.save(todo).toResponse()
+    }
+
 }
--- Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/infra/security/SecurityConfig.kt
+++ Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/infra/security/SecurityConfig.kt
@@ -3,14 +3,19 @@ package com.moveuk.todoapp.infra.security
 import com.moveuk.todoapp.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
-    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint
+    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint,
+    private val customAccessDeniedHandler: CustomAccessDeniedHandler
 ) {
 
     @Bean
@@ -24,14 +29,18 @@ class SecurityConfig(
                     "/login",
                     "/join",
                     "/logout",
+                    "/todos",
+                    "/todos/{todoId}",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
-                ).permitAll()
+                )
+                    .permitAll()
                     .anyRequest().authenticated()
             }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling {
                 it.authenticationEntryPoint(customAuthenticationEntryPoint)
+                    .accessDeniedHandler(customAccessDeniedHandler)
             }
             .build()
     }
