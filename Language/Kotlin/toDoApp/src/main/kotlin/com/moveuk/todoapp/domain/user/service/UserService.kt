8ae92d91d8564bc21b6dbbd21603170a5a41ed66
moveuk <v_donguk@naver.com>
2024-01-17 16:24:05 +0900

    ✨Feat: [Kotlin] ToDoApp add 로그인시 JWT Access Token 반환 및 예외처리
    1. application.yml - auth key 설정
    2. JwtPlugin 구현
        - 토큰 생성, 검증 로직
    3. Security JWT 필터 추가
    4. Security JWT 필터에 사용할 Authentication 구현 및 예외처리 구현
    5. UserPrincipal 구현
    6. SecurityConfig 필터, 예외 처리 추가
    7. UserController login 로직 session 기반에서 jwt 로 변경
    8. UserService jwt 기반 login 로직 구현
    9. UserServiceImpl.kt 삭제 interface 제거
    10. SwaggerConfig jwt authorize 옵션 추가
    11. LoginResponse 구현

--- Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/user/service/UserService.kt
+++ Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/domain/user/service/UserService.kt
@@ -1,8 +1,64 @@
 package com.moveuk.todoapp.domain.user.service
 
+import com.moveuk.todoapp.domain.exception.BadCredentialsException
+import com.moveuk.todoapp.domain.exception.DuplicatedPropertyException
+import com.moveuk.todoapp.domain.exception.EmailNotFoundException
+import com.moveuk.todoapp.domain.todocard.repository.UserRepository
 import com.moveuk.todoapp.domain.user.dto.JoinRequest
+import com.moveuk.todoapp.domain.user.dto.LoginRequest
+import com.moveuk.todoapp.domain.user.dto.LoginResponse
+import com.moveuk.todoapp.domain.user.model.Profile
+import com.moveuk.todoapp.domain.user.model.User
+import com.moveuk.todoapp.infra.security.jwt.JwtPlugin
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
-interface UserService {
-    fun join(joinRequest: JoinRequest): String
+@Service
+class UserService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtPlugin: JwtPlugin
+) {
+    @Transactional
+    fun join(joinRequest: JoinRequest): String {
+        // 회원가입시 이메일 중복 체크 후 유저 정보 DB 저장
+        val (email, password, nickname, role) = joinRequest
 
+        //이메일 중복 체크
+        if (userRepository.existsUserByEmail(email)) throw DuplicatedPropertyException("이메일", email)
+
+        userRepository.save(
+            User(
+                email = email,
+                password = passwordEncoder.encode(password),
+                profile = Profile(nickname = nickname),
+                role = role
+            )
+        )
+        return "회원가입 성공"
+    }
+
+    fun login(loginRequest: LoginRequest): LoginResponse {
+        // 해당 이메일이 DB에 없을시 throw EmailNotFoundException
+        // 비밀번호가 일치하지 않을시 throw BadCredentialsException
+
+        // 이메일 존재하지 않을시 exception throw
+        val user =
+            userRepository.findUserByEmail(loginRequest.email) ?: throw EmailNotFoundException(loginRequest.email)
+        // 역할, 비밀번호 일치하지 않을시
+        if (user.role != loginRequest.role || !passwordEncoder.matches(
+                loginRequest.password,
+                user.password
+            )
+        ) throw BadCredentialsException()
+
+        return LoginResponse(
+            accessToken = jwtPlugin.generateAccessToken(
+                subject = user.id.toString(),
+                email = user.email,
+                role = user.role.name
+            )
+        )
+    }
 }
--- Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/infra/security/SecurityConfig.kt
+++ Language/Kotlin/toDoApp/src/main/kotlin/com/moveuk/todoapp/infra/security/SecurityConfig.kt
@@ -1,12 +1,17 @@
 package com.moveuk.todoapp.infra.security
 
+import com.moveuk.todoapp.infra.security.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val customAuthenticationEntryPoint: CustomAuthenticationEntryPoint
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -23,6 +28,10 @@ class SecurityConfig {
                     "/v3/api-docs/**",
                 ).permitAll()
                     .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling {
+                it.authenticationEntryPoint(customAuthenticationEntryPoint)
             }
             .build()
     }
