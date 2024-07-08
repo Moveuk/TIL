c1e5bc048b4be685a1ce29a273caff17b1ac4d9a
moveuk <v_donguk@naver.com>
2024-07-08 22:26:21 +0900

    Feat: java Calculator - Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다.

--- Language/JAVA/calculator/src/main/java/calculator/App.java
+++ Language/JAVA/calculator/src/main/java/calculator/App.java
@@ -10,5 +10,9 @@ public class App {
         Integer int1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         Integer int2 = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        char operator = sc.next().charAt(0);
+//        char operator = sc.nextLine().charAt(0);  // nextLine()은 개행문자(\n)을 포함하여 가져오기 때문에 charAt 메소드에서 StringIndexOutOfBoundsException이 터짐
     }
 }
