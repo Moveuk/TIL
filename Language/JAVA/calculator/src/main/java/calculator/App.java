fd4e1cd8de55ae8311fc282277722f8493122f14
moveuk <v_donguk@naver.com>
2024-07-09 14:20:31 +0900

    Feat: java Calculator - 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.

--- Language/JAVA/calculator/src/main/java/calculator/App.java
+++ Language/JAVA/calculator/src/main/java/calculator/App.java
@@ -6,45 +6,49 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        Integer int1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        Integer int2 = sc.nextInt();
+        do {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            Integer int1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            Integer int2 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        char operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+            char operator = sc.next().charAt(0);
 //        char operator = sc.nextLine().charAt(0);  // nextLine()은 개행문자(\n)을 포함하여 가져오기 때문에 charAt 메소드에서 StringIndexOutOfBoundsException이 터짐
 
-        int result = 0;
-        boolean errorFlag = false;
-        String message = "에러 메세지";
+            int result = 0;
+            boolean errorFlag = false;
+            String message = "에러 메세지";
 
-        switch (operator) {
-            case '+':
-                result = int1 + int2;
-                break;
-            case '-':
-                result = int1 - int2;
-                break;
-            case '*':
-                result = int1 * int2;
-                break;
-            case '/':
-                if (int2 == 0) {
-                    message = "나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.";
-                    errorFlag = true;
+            switch (operator) {
+                case '+':
+                    result = int1 + int2;
                     break;
-                }
-                result = int1 / int2;
-                break;
-            default:
-                break;
-        }
+                case '-':
+                    result = int1 - int2;
+                    break;
+                case '*':
+                    result = int1 * int2;
+                    break;
+                case '/':
+                    if (int2 == 0) {
+                        message = "나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.";
+                        errorFlag = true;
+                        break;
+                    }
+                    result = int1 / int2;
+                    break;
+                default:
+                    break;
+            }
 
-        if (errorFlag) {
-            System.out.println(message);
-        } else {
-            System.out.println("결과: " + result);
-        }
+            if (errorFlag) {
+                System.out.println(message);
+            } else {
+                System.out.println("결과: " + result);
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+        } while (!sc.next().equals("exit"));
     }
 }
