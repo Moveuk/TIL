93c32fce37b9a1d6b5033a66ef150f0b50ffca3b
moveuk <v_donguk@naver.com>
2024-07-09 16:42:05 +0900

    Feat: java Calculator - Level 1에서 구현한 App 클래스의 main 메서드에 Calculator 클래스가 활용될 수 있도록 수정합니다.
    - 연산 수행 역할은 Calculator 클래스가 담당합니다.
        - 연산 결과는 Calculator 클래스의 연산 결과를 저장하는 필드에 저장됩니다.
    - 소스 코드 수정 후에도 수정 전의 기능들이 반드시 똑같이 동작해야합니다.

--- Language/JAVA/calculator/src/main/java/calculator/App.java
+++ Language/JAVA/calculator/src/main/java/calculator/App.java
@@ -1,70 +1,42 @@
 package calculator;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class App {
     public static void main(String[] args) {
-        Deque<Integer> resultsMemory = new ArrayDeque<>();
+        Calculator calculator = new Calculator();
+
         Scanner sc = new Scanner(System.in);
 
         do {
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            Integer int1 = sc.nextInt();
+            int num1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요: ");
-            Integer int2 = sc.nextInt();
+            int num2 = sc.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
             char operator = sc.next().charAt(0);
-//        char operator = sc.nextLine().charAt(0);  // nextLine()은 개행문자(\n)을 포함하여 가져오기 때문에 charAt 메소드에서 StringIndexOutOfBoundsException이 터짐
 
-            int result = 0;
-            boolean errorFlag = false;
-            String message = "에러 메세지";
-
-            switch (operator) {
-                case '+':
-                    result = int1 + int2;
-                    break;
-                case '-':
-                    result = int1 - int2;
-                    break;
-                case '*':
-                    result = int1 * int2;
-                    break;
-                case '/':
-                    if (int2 == 0) {
-                        message = "나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.";
-                        errorFlag = true;
-                        break;
-                    }
-                    result = int1 / int2;
-                    break;
-                default:
-                    break;
+            try {
+                int result = calculator.calculate(num1, num2, operator);
+                System.out.println("결과: " + result);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
 
-            if (errorFlag) {
-                System.out.println(message);
-            } else {
-                System.out.println("결과: " + result);
-                resultsMemory.addLast(result);
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            if (sc.next().equals("remove")) {
+                calculator.results.pop();
+            }
 
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                if (sc.next().equals("remove")) {
-                    resultsMemory.pop();
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            if (sc.next().equals("inquiry")) {
+                StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
+                for (int i : calculator.results) {
+                    stringJoiner.add(Integer.toString(i));
                 }
-
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                if (sc.next().equals("inquiry")) {
-                    StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
-                    for (int i : resultsMemory) {
-                        stringJoiner.add(Integer.toString(i));
-                    }
-                    System.out.println(stringJoiner);
-                }
+                System.out.println(stringJoiner);
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
