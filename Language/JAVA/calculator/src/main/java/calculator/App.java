7874ff242a50d340cda9fc9f1fa57ebd482dce36
moveuk <v_donguk@naver.com>
2024-07-09 15:29:35 +0900

    Feat: java Calculator - “inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력합니다.
    - foreach(향상된 for문)을 활용하여 구현 해봅니다.

--- Language/JAVA/calculator/src/main/java/calculator/App.java
+++ Language/JAVA/calculator/src/main/java/calculator/App.java
@@ -3,6 +3,7 @@ package calculator;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class App {
     public static void main(String[] args) {
@@ -49,13 +50,20 @@ public class App {
                 System.out.println(message);
             } else {
                 System.out.println("결과: " + result);
-
                 resultsMemory.addLast(result);
 
                 System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-
                 if (sc.next().equals("remove")) {
                     resultsMemory.pop();
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if (sc.next().equals("inquiry")) {
+                    StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
+                    for (int i : resultsMemory) {
+                        stringJoiner.add(Integer.toString(i));
+                    }
+                    System.out.println(stringJoiner);
                 }
             }
 
