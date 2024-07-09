f62f9916a108989d10276ec6d5cf05c895697269
moveuk <v_donguk@naver.com>
2024-07-09 17:20:59 +0900

    Feat: java Calculator - Calculator 클래스에 반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현합니다.
    - APP 클래스의 main 메서드에 Scanner를 활용하여 사칙연산을 진행할지 원의 넓이를 구할지 명령어를 입력 받은 후 원의 넓이를 구하는 것을 선택했을 때 원의 반지름을 입력 받아 원의 넓이를 구한 후 출력되도록 구현합니다.
        - 기존에 구현되어있던 사칙연산 기능은 수정 후에도 반드시 이전과 동일하게 동작해야합니다.
    - 이때, static, final 키워드를 활용할 수 있는지 고민한 후 활용 해봅니다.
        - 반드시 static, final 키워드에 대한 설명과 활용한 이유에 대해 주석으로 작성합니다.
    - 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성
        - 계산된 원의 넓이를 저장합니다.
        - 생성자로 초기화됩니다.
        - 외부에서 직접 접근할 수 없습니다.
        - Getter, Setter 메서드를 구현합니다.
        - 원의 넓이 결과값들을 조회하는 메서드를 구현합니다.

--- Language/JAVA/calculator/src/main/java/calculator/App.java
+++ Language/JAVA/calculator/src/main/java/calculator/App.java
@@ -5,34 +5,58 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(new ArrayDeque<>());
+        Calculator calculator = new Calculator(new ArrayDeque<>(), new ArrayDeque<>());
 
         Scanner sc = new Scanner(System.in);
 
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            System.out.print("사칙연산을 진행한다면 1을, 원의 너비를 구한다면 2를 입력해주세요: ");
+            int option = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
-            char operator = sc.next().charAt(0);
+            switch (option) {
+                case 1 -> {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    int num1 = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int num2 = sc.nextInt();
 
-            try {
-                int result = calculator.calculate(num1, num2, operator);
-                System.out.println("결과: " + result);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
+                    System.out.print("사칙연산 기호를 입력하세요: "); // +, -, *, /
+                    char operator = sc.next().charAt(0);
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (sc.next().equals("remove")) {
-                calculator.removeResult();
-            }
+                    try {
+                        double result = calculator.calculate(num1, num2, operator);
+                        System.out.println("결과: " + result);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if (sc.next().equals("inquiry")) {
-                calculator.inquiryResults();
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    if (sc.next().equals("remove")) {
+                        calculator.removeBasicResult();
+                    }
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    if (sc.next().equals("inquiry")) {
+                        calculator.inquiryBasicResults();
+                    }
+                }
+                case 2 -> {
+                    System.out.print("반지름 숫자를 입력하세요: ");
+                    int radius = sc.nextInt();
+
+                    double result = calculator.calculateCircleArea(radius);
+                    System.out.println("결과: " + result);
+
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    if (sc.next().equals("remove")) {
+                        calculator.removeCircleAreaResult();
+                    }
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    if (sc.next().equals("inquiry")) {
+                        calculator.inquiryCircleAreaResults();
+                    }
+                }
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- Language/JAVA/calculator/src/main/java/calculator/Calculator.java
+++ Language/JAVA/calculator/src/main/java/calculator/Calculator.java
@@ -4,44 +4,73 @@ import java.util.Deque;
 import java.util.StringJoiner;
 
 public class Calculator {
-    private Deque<Integer> results;
+    public static final double PI = 3.14159265358979323846; // 원주율은 여러 인스턴스에서 공용으로 참조하며 상수임.
+    private Deque<Double> basicResults;
+    private Deque<Double> circleAreaResults;
 
-    public Calculator(Deque<Integer> results) {
-        this.results = results;
+    public Calculator(Deque<Double> basicResults, Deque<Double> circleAreaResults) {
+        this.basicResults = basicResults;
+        this.circleAreaResults = circleAreaResults;
     }
 
-    public Integer calculate(int num1, int num2, char operator) throws IllegalArgumentException {
-        int result = switch (operator) {
+    public Double calculate(int num1, int num2, char operator) throws IllegalArgumentException {
+        double result = switch (operator) {
             case '+' -> num1 + num2;
             case '-' -> num1 - num2;
             case '*' -> num1 * num2;
             case '/' -> {
                 if (num2 == 0) throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                yield num1 / num2;
+                yield (double) num1 / num2;
             }
             default -> throw new IllegalArgumentException("연산자 기호는 +, -, *, / 중에만 사용이 가능합니다.");
         };
 
-        results.addLast(result);
+        basicResults.addLast(result);
         return result;
     }
 
-    public Deque<Integer> getResults() {
-        return results;
+    public Deque<Double> getBasicResults() {
+        return basicResults;
     }
 
-    public void setResults(Deque<Integer> results) {
-        this.results = results;
+    public void setBasicResults(Deque<Double> basicResults) {
+        this.basicResults = basicResults;
     }
 
-    public void removeResult() {
-        this.results.pop();
+    public void removeBasicResult() {
+        this.basicResults.pop();
     }
 
-    public void inquiryResults() {
+    public void inquiryBasicResults() {
         StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
-        for (int i : results) {
-            stringJoiner.add(Integer.toString(i));
+        for (double d : basicResults) {
+            stringJoiner.add(Double.toString(d));
+        }
+        System.out.println(stringJoiner);
+    }
+
+    public Double calculateCircleArea(int radius) {
+        double result = radius * radius * PI;
+        circleAreaResults.addLast(result);
+        return result;
+    }
+
+    public Deque<Double> getCircleAreaResults() {
+        return circleAreaResults;
+    }
+
+    public void setCircleAreaResults(Deque<Double> circleAreaResults) {
+        this.circleAreaResults = circleAreaResults;
+    }
+
+    public void removeCircleAreaResult() {
+        this.circleAreaResults.pop();
+    }
+
+    public void inquiryCircleAreaResults() {
+        StringJoiner stringJoiner = new StringJoiner(", ", "[ ", " ]");
+        for (Double d : circleAreaResults) {
+            stringJoiner.add(Double.toString(d));
         }
         System.out.println(stringJoiner);
     }
