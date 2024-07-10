aa07356d6ea9d890f1177542ce3517e25519cb81
moveuk <v_donguk@naver.com>
2024-07-10 14:50:58 +0900

    Feat: java Calculator - ArithmeticCalculator 클래스에 추가로 나머지 연산(%) 기능을 추가하기 위해 ModOperator 클래스를 만들어 추가합니다.
    - 추가하려고 하니 앞으로 계속 기능이 추가되면 여러 부분의 소스코드를 수정해야 한다는 생각이 들었고 “현재 비효율적인 구조가 아닌가?” 라는 의구심이 들었습니다.
        - 따라서 소스 코드의 변경은 최소화하면서 기능을 쉽게 추가(확장)할 수 있는 방법을 고민 해봅니다. (OCP)
    - 방법을 고민 및 학습하여 적용했을 때 전체적인 소스 코드와 구조의 변경이 발생 했을 겁니다.
        - 최대한 생각한 방법으로 구현 해보세요. 틀린 답은 없습니다. 컴파일에 문제가 없고 기능이 정상적으로 동작 하면 모두 정답입니다.
        - 포기하는 것 보다 본인이 생각한데로 구현해보고 다른 개발자들과 공유 하면서 여러 가지 방법들을 확인 했을 때 실력이 가장 많이 향상됩니다.
    - 마찬가지로 수정 후에도 이전과 똑같이 동작해야합니다.

--- Language/JAVA/calculator/src/main/java/calculator/ArithmeticCalculator.java
+++ Language/JAVA/calculator/src/main/java/calculator/ArithmeticCalculator.java
@@ -4,33 +4,30 @@ import java.util.Deque;
 import java.util.StringJoiner;
 
 public class ArithmeticCalculator extends Calculator {
-    private final AddOperator addOperator;
-    private final SubtractOperator subtractOperator;
-    private final MultiplyOperator multiplyOperator;
-    private final DivideOperator divideOperator;
 
-    public ArithmeticCalculator(Deque<Double> results, AddOperator addOperator, SubtractOperator subtractOperator, MultiplyOperator multiplyOperator, DivideOperator divideOperator) {
+    public ArithmeticCalculator(Deque<Double> results) {
         super(results);
-        this.addOperator = addOperator;
-        this.subtractOperator = subtractOperator;
-        this.multiplyOperator = multiplyOperator;
-        this.divideOperator = divideOperator;
     }
 
-    public Double calculate(int num1, int num2, char operator) throws IllegalArgumentException {
-        double result = switch (operator) {
-            case '+' -> addOperator.operate(num1, num2);
-            case '-' -> subtractOperator.operate(num1, num2);
-            case '*' -> multiplyOperator.operate(num1, num2);
-            case '/' -> {
-                if (num2 == 0) throw new IllegalArgumentException("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                yield (double) divideOperator.operate(num1, num2);
-            }
-            default -> throw new IllegalArgumentException("연산자 기호는 +, -, *, / 중에만 사용이 가능합니다.");
-        };
-
+    // IllegalArgumentException은 RuntimeException 하위 클래스이므로 throws를 명시하지 않아도 된다.(unchecked이므로)
+    public Double calculate(int num1, int num2, char operator) {
+        double result = operatorFactory(operator).operate(num1, num2);
         super.getResults().addLast(result);
         return result;
+    }
+
+    /*
+     operator를 생성하는 책임을 분리하며 Calculator 사용시 operator가 어떻게 생성되는지, 연산되는지에 대한 것은 외부에서 알 필요가 없기 때문에 접근제어자를 private로 설정.
+    */
+    private Operator operatorFactory(char operator) {
+        return switch (operator) {
+            case '+' -> new AddOperator();
+            case '-' -> new SubtractOperator();
+            case '*' -> new MultiplyOperator();
+            case '/' -> new DivideOperator();
+            case '%' -> new ModOperator();
+            default -> throw new IllegalArgumentException("연산자 기호는 +, -, *, /, % 중에만 사용이 가능합니다.");
+        };
     }
 
     @Override
