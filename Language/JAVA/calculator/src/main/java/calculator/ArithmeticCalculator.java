0e909aba8462d929039c02f0540cdd616178aeb5
moveuk <v_donguk@naver.com>
2024-07-10 16:17:43 +0900

    Feat: java Calculator - 현재 사칙연산 계산기는 +, -, *, /, % 이렇게 총 5가지 연산 타입으로 구성되어있습니다.
    - Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리하고 이를 사칙연산 계산기 ArithmeticCalculator 클래스에 활용 해봅니다.

--- Language/JAVA/calculator/src/main/java/calculator/ArithmeticCalculator.java
+++ Language/JAVA/calculator/src/main/java/calculator/ArithmeticCalculator.java
@@ -20,13 +20,13 @@ public class ArithmeticCalculator extends Calculator {
      operator를 생성하는 책임을 분리하며 Calculator 사용시 operator가 어떻게 생성되는지, 연산되는지에 대한 것은 외부에서 알 필요가 없기 때문에 접근제어자를 private로 설정.
     */
     private Operator operatorFactory(char operator) {
-        return switch (operator) {
-            case '+' -> new AddOperator();
-            case '-' -> new SubtractOperator();
-            case '*' -> new MultiplyOperator();
-            case '/' -> new DivideOperator();
-            case '%' -> new ModOperator();
-            default -> throw new IllegalArgumentException("연산자 기호는 +, -, *, /, % 중에만 사용이 가능합니다.");
+        OperatorType operatorType = OperatorType.valueOf(operator);
+        return switch (operatorType) {
+            case ADDITION -> new AddOperator();
+            case SUBTRACTION -> new SubtractOperator();
+            case MULTIPLICATION -> new MultiplyOperator();
+            case DIVISION -> new DivideOperator();
+            case MODULO -> new ModOperator();
         };
     }
 
