dcdbc0345bba225f2d0b1ec14e64093b16b534ed
moveuk <v_donguk@naver.com>
2024-11-23 16:16:23 +0900

    🔥warm up!! BOJ - p11654 : 아스키 코드 풀이 + 테스트 코드 작성

---
+++ Algorithm/Algorithm_proj/src/main/java/com/algorithm_proj/boj/p11654/Main.java
@@ -0,0 +1,14 @@
+package com.algorithm_proj.boj.p11654;
+
+import java.util.Scanner;
+
+public class Main {
+    //https://www.acmicpc.net/problem/11654
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int ch = in.next().charAt(0);
+
+        System.out.print(ch);
+    }
+}
