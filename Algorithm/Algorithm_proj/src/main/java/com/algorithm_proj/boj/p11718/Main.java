55c53a747b5a597ba92d10e6c0ba323e3238904e
moveuk <v_donguk@naver.com>
2024-11-29 09:51:18 +0900

    🔥warm up!! BOJ - p11718 : 그대로 출력하기 풀이 + 테스트 코드 작성

---
+++ Algorithm/Algorithm_proj/src/main/java/com/algorithm_proj/boj/p11718/Main.java
@@ -0,0 +1,17 @@
+package com.algorithm_proj.boj.p11718;
+
+import java.util.Scanner;
+
+public class Main {
+    //https://www.acmicpc.net/problem/11718
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()) {
+            String str = sc.nextLine();
+
+            System.out.print(str);
+        }
+        sc.close();
+    }
+}
